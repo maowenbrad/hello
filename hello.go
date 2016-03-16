d054e183ffe1f673582730e6548bcb3a65c7e07b
maowenbrad <braddmo@BTMCAPWXP039.corrections.wi>
2016-03-16 15:51:32 -0500

    Added Glide files.

--- hello.go
+++ hello.go
@@ -2,8 +2,26 @@ package main
 
 import (
 	"fmt"
+	"github.com/cbroglie/mustache"
 )
 
 func main() {
-	fmt.Printf("Hello, Matthew!!\n")
+
+	fmt.Printf("Hello, Matthew!!!\n")
+
+	output, err := mustache.Render("Hello, {{c}}!!\n", map[string]string{"c": "Matthew"})
+
+	if err != nil {
+		fmt.Printf(err.Error())
+	} else {
+		fmt.Printf(output)
+	}
+
+    // folderPath, err := osext.ExecutableFolder()
+    // if err != nil {
+    //     log.Fatal(err)
+    // }
+    // fmt.Println(folderPath)
+
+
 }
